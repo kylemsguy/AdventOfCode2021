cff15abd4bc46a16771438019f20bdb00f1a0be8
Kyle Zhou <kyzh@google.com>
2021-12-06 21:24:33 -0800

    [Day 7] Add a better solution for part 2

--- day07/day7.py
+++ day07/day7.py
@@ -11,19 +11,42 @@ def part1(data):
 
     return curr_min
 
+
 def part2(data):
     curr_min = 1e99
-    # print(min(data), max(data))
+    # Or you could just use [n(n+1)]/2
     partial = [0]
     for i in range(1, max(data) + 1):
         partial.append(partial[i - 1] + i)
-    print(partial)
+
     for i in range(max(data)+1):
         curr_sum = 0
         # print(i)
         for d in data:
             dist = abs(d - i)
+            # Or you could just use [n(n+1)]/2
             cost = partial[dist]
+            curr_sum += cost
+                
+        if curr_sum < curr_min:
+            curr_min = curr_sum
+
+    return curr_min
+
+
+def part2_smart(data):
+    curr_min = 1e99
+    # Or you could just use [n(n+1)]/2
+    partial = [0]
+    for i in range(1, max(data) + 1):
+        partial.append(partial[i - 1] + i)
+
+    for i in range(max(data)+1):
+        curr_sum = 0
+        # print(i)
+        for d in data:
+            dist = abs(d - i)
+            cost = (dist * (dist + 1)) // 2
             curr_sum += cost
                 
         if curr_sum < curr_min:
@@ -39,5 +62,5 @@ if __name__ == "__main__":
     print(">>>Day 7<<<")
     c = part1(data)
     print(f"Part 1: {c}")
-    c = part2(data)
+    c = part2_smart(data)
     print(f"Part 2: {c}")
